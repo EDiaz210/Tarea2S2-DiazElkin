677fe5d526326c71113510d41c863c2ca14faf03
Luxyz <elkin.diaz@epn.edu.ec>
2024-10-16 21:43:16 -0500

    Initial commit

---
+++ src/Jugador.java
@@ -0,0 +1,76 @@
+public class Jugador
+    String nombre;
+    String posiciónDeJuego;
+    int edad;
+    int numeroCamiseta;
+
+	public Jugador(String nombre, String posiciónDeJuego, int edad, int numeroCamiseta){
+        this.nombre = nombre;
+        this.posicionDeJuego = posicionDeJuego;
+        this.edad = edad;
+        this numeroCamiseta = numeroCamiseta;
+    }
+
+
+    public void presentación(){
+
+        System.out.println("El Jugador: "+ nombre);
+        System.out.println("Juega en la posición:  "+ posiciónDeJuego + "\n Tiene: "+edad+" años."+"\n Su numero de 	camiseta es: ");
+
+
+        public void main (System []args){
+
+            Jugador jugador1 = new Jugador;
+            Jugador jugador2 = new Jugador;
+            Jugador jugador3 = new Jugador;
+
+
+
+            jugador1(Elkin Diaz, ,MCO, 20, 21)
+            jugador2(Paco Montes, ,DC, 19, 21)
+            jugador3(Elkin Diaz, ,MCO, 20, 21)
+
+
+
+            jugador1.presentacion();
+            jugador2.presentacion();
+            jugador3.presentacion();
+
+
+
+
+            Scanner sc = new Scanner(System.in)
+            Jugador j1 = new Jugador;
+
+            System.out.println("Ingrese el nombre del jugador: ");
+            nombre = sc.nextLine();
+
+            System.out.println("Ingrese la posición en que juega: ");
+            posiciónDeJuego = sc.nextLine();
+
+            System.out.println("Ingrese la edad del jugador:  ");
+            edad = sc.nextInt();
+
+            System.out,println("Ingrese el número de camiseta: ");
+            numeroCamiseta= sc.nextInt();
+
+            jugador1.presentacion();
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+}
